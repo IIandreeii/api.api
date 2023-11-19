cab54700f1c40112cd236d74b68c17d32e9b0a98
IIandreeii <104792076+IIandreeii@users.noreply.github.com>
2023-11-18 23:49:53 -0600

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,9 @@ var _dotenv = require("dotenv");
 (0, _dotenv.config)();
 console.log(process.env.NICKNAME);
 var config = exports.config = {
-  host: 'localhost',
+  host: 'monorail.proxy.rlwy.net', 
   user: 'root',
-  password: 'andree',
-  database: 'Luis'
+  password: 'AE-a-eh3de264a2D3Ed62eGcc-ECHC-2',
+  database: 'railway',
+  port: 27687
 };
